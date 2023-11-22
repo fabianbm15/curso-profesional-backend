4f527c5dfd90a42a990605f98cf1529f6630ebf0
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-11-22 06:57:11 -0500

    Query Parameters

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hola mundo desde Express");
+app.get("/saludo", function (req, res) {
+  res.send(`Hola ${req.query.name}`);
 });
 
 app.listen(3000);
